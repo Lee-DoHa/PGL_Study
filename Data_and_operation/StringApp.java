86eb9d1a0c01f72029a430074a696bfcd26cf0ac
Lee-DoHa <johnny550@naver.com>
2022-01-13 15:07:51 +0900

    Update StringApp.java

--- Data_and_operation/StringApp.java
+++ Data_and_operation/StringApp.java
@@ -5,7 +5,10 @@ public class StringApp {
 		
 		System.out.println("Hello World!"); //String
 		System.out.println('H'); //Character
-		
+		System.out.println("Hello "
+				+ "World!"); //코드 작성중에 임의로 줄바꿈하면 중간에 + 넣어야함
+		System.out.println("Hello \n World"); //'\n' = 줄바꿈 특수문자
+		System.out.println("Hello \"World\""); //\가 뒤에 따라오는 임무가 있는 문자를 임무 해제함(escape)
 		
 	}
 
