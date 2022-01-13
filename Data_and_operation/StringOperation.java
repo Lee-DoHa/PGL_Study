455f5f50daa07e7cdceafc6999909a1a33b4c634
Lee-DoHa <johnny550@naver.com>
2022-01-13 15:15:04 +0900

    Create StringOperation.java

---
+++ Data_and_operation/StringOperation.java
@@ -0,0 +1,10 @@
+
+public class StringOperation {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!".length()); //.length() 문자열의 길이 return 
+		System.out.println("Hello [[[name]]]!".replace("[[[name]]]", "doha")); //.replace 문자열 대체
+	}
+
+}
