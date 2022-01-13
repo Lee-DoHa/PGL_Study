e9772f0de067bc8483f1ff5602d0b151f494867d
Lee-DoHa <johnny550@naver.com>
2022-01-13 15:27:35 +0900

    Create Variable.java

---
+++ Data_and_operation/Variable.java
@@ -0,0 +1,13 @@
+
+public class Variable {
+
+	public static void main(String[] args) {
+		
+		int a = 1; // integer(정수)
+		double b = 1.1; // real number(실수)
+		String c = "Hello World"; // String(문자열)
+		
+
+	}
+
+}
