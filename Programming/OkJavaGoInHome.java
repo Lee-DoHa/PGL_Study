b8d8596527aed5e828838d0128c1f8e1f05ae97b
Lee-DoHa <johnny550@naver.com>
2022-01-14 18:09:31 +0900

    Update OkJavaGoInHome.java

--- Programming/OkJavaGoInHome.java
+++ Programming/OkJavaGoInHome.java
@@ -8,19 +8,21 @@ public class OkJavaGoInHome {
 
 	public static void main(String[] args) {
 		
+		String id = "JAVA APT 507";
+		
 		//Elevator call
-		Elevator myElevator = new Elevator("JAVA APT 507");
+		Elevator myElevator = new Elevator(id);
 		myElevator.callForUp(1);
 		
 		//Security off
-		Security mySecurity = new Security("JAVA APT 507");
+		Security mySecurity = new Security(id);
 		mySecurity.off();
 		
 		//Light on
-		Lighting hallLamp = new Lighting("JAVA APT 507 / Hall Lamp");
+		Lighting hallLamp = new Lighting(id + " / Hall Lamp");
 		hallLamp.on();
 		
-		Lighting floorLamp = new Lighting("JAVA APT 507 / Floor Lamp");
+		Lighting floorLamp = new Lighting(id + " / Floor Lamp");
 		floorLamp.on();
 		
 	}
